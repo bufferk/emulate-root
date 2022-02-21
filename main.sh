d94f322e01e68dc27e2ab8cb6cb818487bb632b8
bufferk <82165524+bufferk@users.noreply.github.com>
2022-02-22 00:08:53 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,13 +1,13 @@
 echo starting the root accsses
 echo starting... 0%
-wget https://updatepakager.legamer4.repl.co/root/yt.zip
+wget https://github.com/bufferk/emulate-root/raw/master/yt.zip
 unzip yt.zip  
 echo starting... 25%
 unzip root.zip
 tar -xvf root.tar.xz
 echo starting... 30%
-wget https://updatepakager.legamer4.repl.co/root/up-new.sh
-wget https://updatepakager.legamer4.repl.co/root/root2-new.sh
+wget https://github.com/bufferk/emulate-root/raw/master/up-new.sh
+wget https://github.com/bufferk/emulate-root/raw/master/up-new.zip
 echo starting... 35%
 mv root2-new.sh root.sh
 mv up-new.sh up.sh
